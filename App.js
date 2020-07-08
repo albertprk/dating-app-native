267ace44e8e64980755350dbf023fa3395c874d3
Dallas Hermanson <inboxDallas@gmail.com>
2020-07-08 13:04:21 -0700

    some refactoring

--- App.js
+++ App.js
@@ -1,88 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Switch, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+const orientations = [
+  'Straight',
+  'Gay',
+  'Lesbian',
+  'Bisexual',
+  'Asexual',
+]
 
-  render() {
-    return (
-        <View style={styles.container}>
-          <Header />
-          <View style={styles.orientationForm}>
-            <Text style={styles.headerText}>My sexual {'\n'}orientation is</Text>
-            <Text style={styles.orientationInstruction}>Choose one</Text>
-            <OrientationOption option="Straight" />
-            <OrientationOption option="Gay" />
-            <OrientationOption option="Lesbian" />
-            <OrientationOption option="Bisexual" />
-            <OrientationOption option="Asexual" />
-          </View>
-          <View style={styles.continueButton}><Button color="white" title="CONTINUE" /></View>
-        </View>
-    );
-  }
+function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.orientationForm}>
+        <Text style={styles.headerText}>My sexual {'\n'}orientation is</Text>
+        <Text style={styles.orientationInstruction}>Choose one</Text>
+        {orientations.map((orientation) => (
+          <OrientationOption key={orientation} option={orientation} />
+        ))}
+      </View>
+      <View style={styles.continueButton}>
+        <Text style={styles.continueButtonText}>Continue</Text>
+      </View>
+    </View>
+  )
 }
 
-class Header extends Component {
-  render() {
-    return (
-        <View style={styles.screenHeader}>
-          <View style={styles.back}><Text style={styles.backButton}>{"<"}</Text></View>
-          <View style={styles.skip}><Text style={styles.skipButton}>SKIP</Text></View>
-        </View>
-    );
-  }
+export default App;
+
+function Header() {
+  return (
+    <View style={styles.screenHeader}>
+      <View style={styles.back}>
+        <Text style={styles.backButton}>
+          {"<"}
+        </Text>
+      </View>
+      <View style={styles.skip}>
+        <Text style={styles.skipButton}>
+          SKIP
+        </Text>
+      </View>
+    </View>
+  )
 }
 
 class OrientationOption extends Component {
   constructor() {
     super();
     this.state = {
-      selected: "false",
+      selected: false,
     }
   }
 
-  handlePress = () => {
-    if (this.state.selected === "false") {
-      this.setState({
-        selected: "true"
-      })
-    } else {
-      this.setState({
-        selected: "false"
-      })
-    }
+  _handlePress = () => {
+    const { selected } = this.state;
+    this.setState(prevState => ({
+      selected: !prevState.selected,
+    }));
   }
 
   render() {
-    if (this.state.selected === "true") {
-      return (
-          <View style={styles.checkForm}>
-            <View style={styles.orientationButton}>
-              <Button color="black" title={this.props.option} onPress={this.handlePress} />
-            </View>
-            <Image style={styles.check} source={require("./check.png")} />
-          </View>
-      )
-    } else {
-      return (
-          <View style={styles.checkForm}>
-            <View style={styles.orientationButton}>
-              <Button color="black" title={this.props.option} onPress={this.handlePress} />
-            </View>
-          </View>
-      )
-    }
+    const { option } = this.props;
+    const { selected } = this.state;
+    return (
+      <View style={styles.checkForm}>
+        <View style={styles.orientationButton}>
+          <Button
+            color="black"
+            title={option}
+            onPress={this._handlePress}
+          />
+        </View>
+        {selected ? <Image style={styles.check} source={require("./check.png")} /> : null}
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    background: 'none',
+    backgroundColor: 'transparent',
     alignItems: 'center',
   },
   headerText: {
@@ -124,14 +124,14 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   checkForm: {
-    alignItems: 'left',
+    alignItems: 'flex-start',
     flexDirection: 'row',
     marginTop: 3,
     marginLeft: 10,
   },
   orientationButton: {
-    background: 'white',
-    alignItems: 'left',
+    backgroundColor: 'white',
+    alignItems: 'flex-start',
     width: '60%',
   },
   check: {
@@ -142,11 +142,15 @@ const styles = StyleSheet.create({
   },
   continueButton: {
     backgroundColor: "#00b87e",
-    paddingTop: 3,
+    width: "65%",
+    height: 50,
+    borderRadius: 28,
+  },
+  continueButtonText: {
+    color: 'white',
     fontSize: 15,
     fontWeight: "700",
-    width: "65%",
-    height: 45,
-    borderRadius: 28,
+    textAlign: 'center',
+    lineHeight: 50,
   }
 });
