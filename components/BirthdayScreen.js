402a09466adfee66666155490cd6f9cc28ed11c4
albertprk <thealbertpk@gmail.com>
2020-07-12 15:48:46 -0700

    work on birthdayscreen

--- components/BirthdayScreen.js
+++ components/BirthdayScreen.js
@@ -15,57 +15,99 @@ class BirthdayScreen extends Component {
                 <Text style={styles.mainText}>My birthday is</Text>
                 <View style={styles.birthdayInputContainer}>
                     <TextInput
+                        ref={refOne => this.refOne = refOne}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="Y"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refTwo.focus()}
                     />
                     <TextInput
+                        ref={refTwo => this.refTwo = refTwo}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="Y"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refThree.focus()}
                     />
                     <TextInput
+                        ref={refThree => this.refThree = refThree}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="Y"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refFour.focus()}
                     />
                     <TextInput
+                        ref={refFour => this.refFour = refFour}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="Y"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refFive.focus()}
                     />
                     <Text style={styles.birthdayEntrySeparator}>/</Text>
                     <TextInput
+                        ref={refFive => this.refFive = refFive}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="M"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refSix.focus()}
                     />
                     <TextInput
+                        ref={refSix => this.refSix = refSix}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="M"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refSeven.focus()}
                     />
                     <Text style={styles.birthdayEntrySeparator}>/</Text>
                     <TextInput
+                        ref={refSeven => this.refSeven = refSeven}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="D"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refEight.focus()}
                     />
                     <TextInput
+                        ref={refEight => this.refEight = refEight}
+                        keyboardType="numeric"
                         maxLength={1}
                         placeholder="D"
                         style={styles.birthdayInputField}
+                        clearTextOnFocus={true}
+                        onChangeText={() => this.refEight.focus()}
                     />
                 </View>
-                <Text style={styles.subText}>
-                    Your age will be public
-                </Text>
+                <Text style={styles.subText}>Your age will be public</Text>
             </View>
         );
     }
 }
 
 export default BirthdayScreen;
+
+const NumericTextInputField = (props) => {
+    return (
+        <TextInput
+            keyboardType="numeric"
+            maxLength={1}
+            placeholder={props.placeholder}
+            style={styles.birthdayInputField}
+            onChangeText={() => this.refTwo.focus()}
+        />
+    );
+}
 
 const styles = StyleSheet.create({
     container: {
